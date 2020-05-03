8c678242ef5cfbc983013e01f79a5315dcb34caf
Jeff C <blackone787@gmail.com>
2020-05-03 17:42:14 -0500

    Add reducer and types for handling options like lowercase, uppercase, special characters and numeric.

--- src/components/PasswordGenerator.container.tsx
+++ src/components/PasswordGenerator.container.tsx
@@ -1,20 +1,41 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, Reducer } from 'react';
 import PasswordGeneratorView from './PasswordGenerator.view';
 
-type State = {
+export interface State {
   length: number;
   lowercase: boolean;
   uppercase: boolean;
   numeric: boolean;
   specialCharacter: boolean;
   password: string;
-};
+}
 
-type Action = {
-  type: string;
-};
+interface ActionPassword {
+  type: 'password';
+  password: string;
+}
 
-const initialState = {
+interface ActionToggleLowercase {
+  type: 'toggleLowercase';
+}
+interface ActionToggleUpperCase {
+  type: 'toggleUppercase';
+}
+interface ActionToggleNumeric {
+  type: 'toggleNumeric';
+}
+interface ActionToggleSpecialCharacter {
+  type: 'toggleSpecialCharacter';
+}
+
+type Action =
+  | ActionPassword
+  | ActionToggleLowercase
+  | ActionToggleNumeric
+  | ActionToggleSpecialCharacter
+  | ActionToggleUpperCase;
+
+const initialState: State = {
   length: 8,
   lowercase: false,
   uppercase: false,
@@ -23,20 +44,61 @@ const initialState = {
   password: '',
 };
 
-function reducer(state: State, action: Action) {
+function reducer(state: State, action: Action): State {
   switch (action.type) {
+    case 'toggleSpecialCharacter':
+      return { ...state, specialCharacter: !state.specialCharacter };
+    case 'toggleLowercase':
+      return { ...state, lowercase: !state.lowercase };
+    case 'toggleUppercase':
+      return { ...state, uppercase: !state.uppercase };
+    case 'toggleNumeric':
+      return { ...state, numeric: !state.numeric };
+    case 'password':
+      return { ...state, password: action.password };
     default:
       return state;
   }
 }
 
 const PasswordGeneratorContainer = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  console.log(state, dispatch);
+  const [state, dispatch] = useReducer<Reducer<State, Action>>(
+    reducer,
+    initialState
+  );
   const onClick = () => {
     console.log('Button Clicked');
   };
-  return <PasswordGeneratorView onClick={onClick} password={state.password} />;
+  const toggleLowercase = () => {
+    dispatch({
+      type: 'toggleLowercase',
+    });
+  };
+  const toggleUppercase = () => {
+    dispatch({
+      type: 'toggleUppercase',
+    });
+  };
+  const toggleNumeric = () => {
+    dispatch({
+      type: 'toggleNumeric',
+    });
+  };
+  const toggleSpecialCharacter = () => {
+    dispatch({
+      type: 'toggleSpecialCharacter',
+    });
+  };
+  return (
+    <PasswordGeneratorView
+      onClick={onClick}
+      state={state}
+      toggleLowercase={toggleLowercase}
+      toggleUppercase={toggleUppercase}
+      toggleNumeric={toggleNumeric}
+      toggleSpecialCharacter={toggleSpecialCharacter}
+    />
+  );
 };
 
 export default PasswordGeneratorContainer;
--- src/components/PasswordGenerator.view.tsx
+++ src/components/PasswordGenerator.view.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { State } from './PasswordGenerator.container';
 
 type PasswordGeneratorViewProps = {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  password: string;
+  state: State;
+  toggleLowercase: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  toggleNumeric: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  toggleUppercase: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  toggleSpecialCharacter: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 const PasswordGeneratorView = (props: PasswordGeneratorViewProps) => {
-  const { onClick, password } = props;
+  const {
+    onClick,
+    toggleLowercase,
+    toggleNumeric,
+    toggleSpecialCharacter,
+    toggleUppercase,
+    state,
+  } = props;
+  const {
+    password,
+    numeric,
+    specialCharacter,
+    lowercase,
+    uppercase,
+    length,
+  } = state;
   return (
     <>
       <div className="row">
@@ -15,21 +35,45 @@ const PasswordGeneratorView = (props: PasswordGeneratorViewProps) => {
         </div>
       </div>
       <div className="row">
-        <div className="col-3">
-          <input type="checkbox" name="" id="" />
+        <div className="col-2">
+          <input
+            type="checkbox"
+            id="specialCharacter"
+            onChange={toggleSpecialCharacter}
+            checked={specialCharacter}
+          />
           <label>Special Characters</label>
         </div>
-        <div className="col-3">
-          <input type="checkbox" name="" id="" />
+        <div className="col-2">
+          <input
+            type="checkbox"
+            id="lowercase"
+            onChange={toggleLowercase}
+            checked={lowercase}
+          />
           <label>Lowercase</label>
         </div>
-        <div className="col-3">
-          <input type="checkbox" name="" id="" />
-          <label>UpperCase</label>
+        <div className="col-2">
+          <input
+            type="checkbox"
+            id="uppercase"
+            onChange={toggleUppercase}
+            checked={uppercase}
+          />
+          <label>Uppercase</label>
         </div>
-        <div className="col-3">
+        <div className="col-2">
+          <input
+            type="checkbox"
+            id="numeric"
+            onChange={toggleNumeric}
+            checked={numeric}
+          />
+          <label>Numeric</label>
+        </div>
+        <div className="col-4">
           <label>Password Length:</label>
-          <input type="number" name="" id="" />
+          <input type="number" id="length" value={length} />
         </div>
       </div>
       <div className="row">
