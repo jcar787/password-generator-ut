f27ac4fb7bc711661682bad35cd3dbd7f4225051
Jeff C <blackone787@gmail.com>
2020-05-02 23:22:36 -0500

    Create the workflow to start generating a password.

--- src/components/PasswordGenerator.container.tsx
+++ src/components/PasswordGenerator.container.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import PasswordGeneratorView from './PasswordGenerator.view';
 
+type State = {
+  length: number;
+  lowercase: boolean;
+  uppercase: boolean;
+  numeric: boolean;
+  specialCharacter: boolean;
+  password: string;
+};
+
+type Action = {
+  type: string;
+};
+
+const initialState = {
+  length: 8,
+  lowercase: false,
+  uppercase: false,
+  numeric: false,
+  specialCharacter: false,
+  password: '',
+};
+
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
 const PasswordGeneratorContainer = () => {
-  return <PasswordGeneratorView />;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state, dispatch);
+  const onClick = () => {
+    console.log('Button Clicked');
+  };
+  return <PasswordGeneratorView onClick={onClick} password={state.password} />;
 };
 
 export default PasswordGeneratorContainer;
--- src/components/PasswordGenerator.view.tsx
+++ src/components/PasswordGenerator.view.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const PasswordGeneratorView = () => {
+type PasswordGeneratorViewProps = {
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  password: string;
+};
+
+const PasswordGeneratorView = (props: PasswordGeneratorViewProps) => {
+  const { onClick, password } = props;
   return (
     <>
       <div className="row">
@@ -9,7 +15,36 @@ const PasswordGeneratorView = () => {
         </div>
       </div>
       <div className="row">
-        <div className="col-12">This is the password generator</div>
+        <div className="col-3">
+          <input type="checkbox" name="" id="" />
+          <label>Special Characters</label>
+        </div>
+        <div className="col-3">
+          <input type="checkbox" name="" id="" />
+          <label>Lowercase</label>
+        </div>
+        <div className="col-3">
+          <input type="checkbox" name="" id="" />
+          <label>UpperCase</label>
+        </div>
+        <div className="col-3">
+          <label>Password Length:</label>
+          <input type="number" name="" id="" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <div className="text-center">
+            <button className="btn btn-primary" onClick={onClick}>
+              Generate Password
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <div className="text-center">{password}</div>
+          </div>
+        </div>
       </div>
     </>
   );
